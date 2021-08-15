561b40cb8a5017ed718f26fb44fe6c8e0f82d5bf
me-good-programmer <lenabelov76@gmail.com>
2021-08-15 20:50:25 +0300

    gvbhgcou

--- js/arcanoid.js
+++ js/arcanoid.js
@@ -106,13 +106,12 @@ class Ball {
 
 	collideBricks() {
 		for (var i = 0; i < bricks.length; i++) {
-			if ((this.x + this.width > bricks[i].x)&& (this.x + this.width < bricks[i].x + 42)) || (this.y + this.height > bricks[i].y)&& (this.x + this.height < bricks[i].y + 20){
-				this.dx = - this.dx
+			
+			if ((this.y + this.dy > bricks[i].y)&& (this.y + this.dy < bricks[i].y + 20)){
+				this.dy = - this.dy
 				bricks[i].x = 1000
 			}
 			
-			
-		
 		}	
 	}
 }
